7c7d8a75af690510fe4a671257306c32f4d76734
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2020-02-15 00:43:29 +0100

    Creating countdown and displaying it on index

--- js/spacexData.js
+++ js/spacexData.js
@@ -1,12 +1,11 @@
 
 // Objects containing section specific data
 const sectionObjects = (function() {
-    // Last mission section (index page)
+    // Last mission section object (index page)
     const lastMissionSection = {
         parentContainer: document.querySelector('#last-mission .item-container'),
         createElements: function(data) {
-            const parentContainer = document.querySelector('#last-mission .item-container'),
-                flexDiv = document.createElement('div'),
+            const flexDiv = document.createElement('div'),
                 itemImg = document.createElement('div'),
                 itemDetails = document.createElement('div'),
                 itemButton = document.createElement('div');
@@ -47,10 +46,33 @@ const sectionObjects = (function() {
             // Append elements to DOM
             flexDiv.appendChild(itemImg);
             flexDiv.appendChild(itemDetails);
-            parentContainer.appendChild(flexDiv);
-            parentContainer.appendChild(itemButton);
+            this.parentContainer.appendChild(flexDiv);
+            this.parentContainer.appendChild(itemButton);
         }
-    };
+    }, 
+    // Countdown section object
+        countdownSection = {
+            parentContainer: document.querySelector('#countdown #time-left'),
+            createElements: function(data) {
+                const parentContainer = this.parentContainer,
+                    launchTime = new Date(data.launch_date_local).getTime();
+                // Create the timer
+                let countdown = setInterval(function() {
+                    const now = new Date().getTime();
+                    // Calculate remaining time
+                    let timeLeft = launchTime - now,
+                        days = Math.floor(timeLeft / (1000 * 60 * 60 * 24)),
+                        hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                        minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+                        seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+                    // Display countdown on page
+                    parentContainer.innerHTML = `
+                        <p><span>${days}</span> days <span>${hours}</span> hours <span>${minutes}</span> minutes <span>${seconds}</span> seconds</p>
+                    `;
+                }, 1000);   
+            }
+        };
 
     // Convert dates to strings
     function dateConverter(date) {
@@ -59,6 +81,7 @@ const sectionObjects = (function() {
     };
 
     return {
+        countdownSection,
         lastMissionSection
     }
 })();
@@ -74,7 +97,8 @@ function loadAPI(apiURL, targetSection) {
 };
 // Receive data from API
 function getData(data, targetSection) {
-    displayContent(data, targetSection); 
+    displayContent(data, targetSection);
+    console.log(data);
 }
 // Display elements on page
 function displayContent(data, section) {
@@ -84,12 +108,14 @@ function displayContent(data, section) {
 }
 
 (function() {
+    // Check if 'last-mission' section is present
     if (sectionObjects.lastMissionSection.parentContainer) {
-        console.log("It exists");
+        console.log("last-mission exists");
         loadAPI("launches/latest", sectionObjects.lastMissionSection);
-        // displayContent(lastMissionSection);
-
-    } else {
-        console.log("It does not exist");
+    }
+    // Check if 'countdown' section is present
+    if (sectionObjects.countdownSection.parentContainer) {
+        console.log("Countdown exists");
+        loadAPI("launches/next", sectionObjects.countdownSection);
     }
 })();
