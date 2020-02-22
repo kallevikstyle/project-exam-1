81175a918578d8c881bc5203484eb5a344f24496
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2020-02-22 17:46:58 +0100

    Adding script for cta-menu

---
+++ js/cta-menu.js
@@ -0,0 +1,19 @@
+const ctaMenu = (function() {
+    let rockets = {
+        divElement: document.querySelector('#cta-rockets'),
+        normalState: "../images/buttons/rockets_normal.png",
+        hoverState: "../images/buttons/rockets_hover.png",
+        activeState: "../images/buttons/rockets_active.png"
+        },
+        launches = {
+            divElement: document.querySelector('#cta-launches'),
+            normalState: "../images/buttons/launches_normal.png",
+            hoverState: "../images/buttons/launches_hover.png",
+            activeState: "../images/buttons/launches_active.png"
+        };
+})();
+
+(function() {
+
+})();
+
