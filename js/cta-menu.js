afb535cff344a9661dfd5799c9901677e58d8f82
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2020-02-22 18:19:35 +0100

    Creating hover and active states for cta-menu

--- js/cta-menu.js
+++ js/cta-menu.js
@@ -1,19 +1,74 @@
 const ctaMenu = (function() {
     let rockets = {
         divElement: document.querySelector('#cta-rockets'),
-        normalState: "../images/buttons/rockets_normal.png",
-        hoverState: "../images/buttons/rockets_hover.png",
-        activeState: "../images/buttons/rockets_active.png"
+        normalState: "url('images/buttons/rockets_normal.png')",
+        hoverState: "url('images/buttons/rockets_hover.png')",
+        activeState: "url('images/buttons/rockets_active.png')"
         },
         launches = {
             divElement: document.querySelector('#cta-launches'),
-            normalState: "../images/buttons/launches_normal.png",
-            hoverState: "../images/buttons/launches_hover.png",
-            activeState: "../images/buttons/launches_active.png"
+            normalState: "url('images/buttons/launches_normal.png')",
+            hoverState: "url('images/buttons/launches_hover.png')",
+            activeState: "url('images/buttons/launches_active.png')"
+        },
+        down = {
+            divElement: document.querySelector('#cta-down'),
+            normalState: "url('images/buttons/down_normal-2.png')",
+            hoverState: "url('images/buttons/down_hover-2.png')",
+            activeState: "url('images/buttons/down_normal.png')",
         };
+
+    return {
+        rockets,
+        launches,
+        down
+    }
 })();
 
 (function() {
-
+    // Hover states for cta buttons
+    // Rockets
+    ctaMenu.rockets.divElement.addEventListener('mouseover', function() {
+        ctaMenu.rockets.divElement.style.backgroundImage = ctaMenu.rockets.hoverState;
+    });
+    ctaMenu.rockets.divElement.addEventListener('mouseout', function() {
+        ctaMenu.rockets.divElement.style.backgroundImage = ctaMenu.rockets.normalState;
+    });
+    // Launches
+    ctaMenu.launches.divElement.addEventListener('mouseover', function() {
+        ctaMenu.launches.divElement.style.backgroundImage = ctaMenu.launches.hoverState;
+    });
+    ctaMenu.launches.divElement.addEventListener('mouseout', function() {
+        ctaMenu.launches.divElement.style.backgroundImage = ctaMenu.launches.normalState;
+    });
+    // Down
+    ctaMenu.down.divElement.addEventListener('mouseover', function() {
+        ctaMenu.down.divElement.style.backgroundImage = ctaMenu.down.hoverState;
+    });
+    ctaMenu.down.divElement.addEventListener('mouseout', function() {
+        ctaMenu.down.divElement.style.backgroundImage = ctaMenu.down.normalState;
+    });
+    // Active states
+    // Rockets
+    ctaMenu.rockets.divElement.addEventListener('mousedown', function() {
+        ctaMenu.rockets.divElement.style.backgroundImage = ctaMenu.rockets.activeState;
+    });
+    ctaMenu.rockets.divElement.addEventListener('mouseup', function() {
+        ctaMenu.rockets.divElement.style.backgroundImage = ctaMenu.rockets.normalState;
+    });
+    // Launches
+    ctaMenu.launches.divElement.addEventListener('mousedown', function() {
+        ctaMenu.launches.divElement.style.backgroundImage = ctaMenu.launches.activeState;
+    });
+    ctaMenu.launches.divElement.addEventListener('mouseup', function() {
+        ctaMenu.launches.divElement.style.backgroundImage = ctaMenu.launches.normalState;
+    });
+    // Down
+    ctaMenu.down.divElement.addEventListener('mousedown', function() {
+        ctaMenu.down.divElement.style.backgroundImage = ctaMenu.down.activeState;
+    });
+    ctaMenu.down.divElement.addEventListener('mouseup', function() {
+        ctaMenu.down.divElement.style.backgroundImage = ctaMenu.down.normalState;
+    });
 })();
 
